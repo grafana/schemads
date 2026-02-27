ff9c9339b05f496b2162aa2be6860702950418c8
Andreas Christou <andreas.christou@grafana.com>
2026-02-27 18:49:52 +0000

    Improve error handling

--- errors.go
+++ errors.go
@@ -12,7 +12,10 @@ var (
 )
 
 func sendSchemaError(sender backend.CallResourceResponseSender, status int, message string) error {
-	body, _ := json.Marshal(map[string]string{"error": message})
+	body, err := json.Marshal(map[string]string{"error": message})
+	if err != nil {
+		body = []byte(`{"error":"internal error"}`)
+	}
 	return sender.Send(&backend.CallResourceResponse{
 		Status:  status,
 		Headers: map[string][]string{"Content-Type": {"application/json"}},
